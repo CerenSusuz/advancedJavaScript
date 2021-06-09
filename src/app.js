6fb4ae01d5860b52724052043a50893dec007b80
CEREN SUSUZ <ceren199704@hotmail.com>
2021-06-09 11:16:54 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,47 @@
+// var numOne = 10;
+// numOne = "Ceren";
+// console.log(numOne); // Ceren
+
+// var numTest = 10;
+// var numTest = "CerenSSZ";
+// console.log(numTest); // CerenSSZ
+
+// let numTwo = 20;
+// let numTwo = "CEREN";
+// console.log(numTwo); // Duplicate declaration, ERROR
+
+// let numThree = 20;
+// numThree = "CERENNN";
+// console.log(numThree); // CERENNN
+
+// student = {id:1,Name:"Ceren"} // object
+// console.log(student); // ReferenceError: student is not defined
+let student = {id:1,Name:"Ceren"}
+// console.log(student); // {id:1,Name:"Ceren"}
+
+// function save() {
+//     console.log("SAVED")
+// }
+
+// save() // SAVED
+
+// function save(student) {
+//     console.log("no params")
+//     console.log(student.Name)
+// }
+
+// save(); // no params
+// save(student);
+// no params
+// Ceren
+
+function show(score=50, student) { // default 50
+    console.log(score)
+}
+
+// show() // 50 
+// show(100) // 100
+show(undefined, student)
+
+
+
